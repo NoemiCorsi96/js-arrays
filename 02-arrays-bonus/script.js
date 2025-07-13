53b84030ac4e8bca996bc9c5ae90edb04a141346
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-07-13 16:50:53 +0200

    do 2.3 and 2.4

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -27,9 +27,39 @@ console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
+teachers.splice(5, 1);
+console.log(teachers);
+// Ho eliminato l'elemento che si trova in posizione 5
+// Ricorda che al momento l'array non è più quello iniziale ma è capovolto
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+
+let verifico = [];
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === 'Fabio') {
+    verifico.push('true');
+    
+  } else {
+    verifico.push('false');
+    
+  }
+}
+console.log(verifico);
+
+const true_index = verifico.indexOf('true');
+let isFabioPresent = ''
+
+if (true_index == -1) {
+  isFabioPresent = 'Fabio non è presente';
+} else {
+  isFabioPresent = 'Fabio è presente';
+}
+
+console.log(isFabioPresent);
+
+
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
